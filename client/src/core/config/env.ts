575a1fe91c336bd9873165800d2720ab600bf226
monkamherman <cesaristos5@gmail.com>
2025-03-16 21:15:01 +0100

    test 1

--- client/src/core/config/env.ts
+++ client/src/core/config/env.ts
@@ -4,7 +4,7 @@ import * as env from 'env-var';
 const { get } = env;
 
 const envs = {
-  PORT: get('PORT').default(4000).asPortNumber(),
+  PORT: get('PORT').default(10000).asPortNumber(),
   NODE_ENV: get('NODE_ENV').default('development').asString(),
   HOST: get('HOST').default('0.0.0.0').asString(),
 };
